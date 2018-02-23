454e934d88f89814f9956a7f14ac04d50aeba38f
SergioSSJ <sjcmexr@gmail.com>
2018-02-22 23:11:39 -0600

    problem with paths solved

---
+++ src/main/java/org/sjimenez/chatapp/controllers/HomeController.java
@@ -0,0 +1,22 @@
+
+
+
+
+   package org.sjimenez.chatapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+    @Controller
+    public class HomeController {
+/*
+        @RequestMapping(value = {"/", "/about"})
+        public String index() {
+            return "index";
+        }
+        */
+    }
+
+
+
+
